598373bfafbdd453dc311bcabdc11bbd0221bd06
GlobalAA <globala977@gmail.com>
2023-06-09 12:49:19 +0300

    Fixed bugs in settings

--- web/js/settings.js
+++ web/js/settings.js
@@ -5,9 +5,13 @@ const outputPathButton = document.getElementById('open-button');
 const dirInput = document.getElementById('dir-input');
 const language = document.getElementById('language');
 const timeout = document.getElementById('timeout');
+const listenPreview = document.getElementById('listen_preview');
+const autoListenPreview = document.getElementById('auto_listen_preview');
+const defaultLanguagePreview = document.getElementById('default_language_preview');
+const timeoutPreview = document.getElementById('timeout_preview');
 const inputs = document.querySelectorAll('[boolean]');
 window.onload = () => {
-    defaultFill(language, dirInput, inputs[0], inputs[1], timeout);
+    defaultFill(language, dirInput, inputs[0], inputs[1], timeout, listenPreview, autoListenPreview, defaultLanguagePreview, timeoutPreview);
     window.resizeTo(420, 570);
 };
 // Output Path
